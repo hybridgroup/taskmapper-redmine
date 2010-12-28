d3133e45e996eb24d4592b7d2b699872c316a61f
Rafael George <george.rafael@gmail.com>
2010-12-28 19:58:15 +0000

    added project creation and update - added issues fixtures - catching the typecast error and returning an empty array - Version bump to 0.1.0 - gemspec - added update of a project - added the ability to create projects

--- spec/projects_spec.rb
+++ spec/projects_spec.rb
@@ -3,10 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Ticketmaster::Provider::Redmine::Project" do
   before(:all) do
     headers = {'Authorization' => 'Basic Y29yZWQ6MTIzNDU2', 'Accept' => 'application/xml'}
+    headers_post_put = {'Authorization' => 'Basic Y29yZWQ6MTIzNDU2', 'Content-Type' => 'application/xml'}
     @project_id = 'test-repo12'
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get '/projects.xml', headers, fixture_for('projects'), 200
       mock.get '/projects/test-repo12.xml', headers, fixture_for('projects/test-repo12'), 200
+      mock.put '/projects/1.xml', headers_post_put, '', 200
+      mock.post '/projects.xml', headers_post_put, '', 200
     end
   end
 
@@ -42,5 +45,14 @@ describe "Ticketmaster::Provider::Redmine::Project" do
   it "should be able to find a project by identifier" do 
     @ticketmaster.project(@project_id).should be_an_instance_of(@klass)
     @ticketmaster.project(@project_id).identifier.should == @project_id
+  end
+
+  it "should be able to update and save a project" do
+    @project = @ticketmaster.project(@project_id)
+    @project.update!(:name => 'named').should == true
+  end
+
+  it "should be able to create a new project" do 
+    @project = @ticketmaster.project!(:name => 'newtest', :identifier => 'dumb', :description => 'hithere').should be_an_instance_of(@klass)
   end
 end
