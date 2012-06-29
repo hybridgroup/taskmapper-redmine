489b440afca225c31b0834acc310449943485da9
Rafael George <george.rafael@gmail.com>
2012-06-29 20:16:27 +0000

    Refactor ticket specs * Added initial code to refactor update specification

--- spec/tickets_spec.rb
+++ spec/tickets_spec.rb
@@ -1,28 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe TaskMapper::Provider::Redmine::Ticket do
-  let(:headers) { {'X-Redmine-API-Key' => 'abcdefghijk', 'Accept' => 'application/xml'} }
-  let(:headers_post_put) { headers.merge('Content-Type' => 'application/xml') }
+  let(:headers_auth) { {'X-Redmine-API-Key' => 'abcdefghijk'} }
+  let(:headers_get) { headers_auth.merge('Accept' => 'application/xml') }
+  let(:headers_post_put) { headers_auth.merge('Content-Type' => 'application/xml') }
   let(:project_id) { 1 }
   let(:tm) { TaskMapper.new :redmine, :server => 'http://demo.redmine.org/', :token => 'abcdefghijk' }
   let(:ticket_class) { TaskMapper::Provider::Redmine::Ticket }
   let(:ticket_id) { 1 }
-  before(:each) do 
-    ActiveResource::HttpMock.respond_to do |mock|
-      mock.get '/projects.xml', headers, fixture_for('projects'), 200
-      mock.get '/issues.xml', headers, fixture_for('issues'), 200
-
-      mock.put '/issues/1.xml', headers_post_put, '', 200
-      mock.post '/issues.xml', headers_post_put, '', 200
-    end
-  end
 
   describe "Retrieving tickets" do 
     before(:each) do 
       ActiveResource::HttpMock.respond_to do |mock|
-        mock.get '/projects/1.xml', headers, fixture_for('projects/test-repo12'), 200
-        mock.get '/issues.xml?include=journals&project_id=1', headers, fixture_for('issues'), 200
-        mock.get '/issues/1.xml?include=journals', headers, fixture_for('issues/1'), 200
+        mock.get '/projects/1.xml', headers_get, fixture_for('projects/test-repo12'), 200
+        mock.get '/issues.xml?include=journals&project_id=1', headers_get, fixture_for('issues'), 200
+        mock.get '/issues/1.xml?include=journals', headers_get, fixture_for('issues/1'), 200
       end
     end
     let(:project) { tm.project project_id }
@@ -60,18 +52,30 @@ describe TaskMapper::Provider::Redmine::Ticket do
     end
   end
 
+  describe "Create and Update" do
+    before(:each) do 
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/projects/1.xml', headers_get, fixture_for('projects/test-repo12'), 200
+        mock.post '/issues.xml', headers_post_put, '', 200
+      end
+    end
+    let(:project) { tm.project project_id }
+
+    context "when #ticket!" do 
+      subject { project.ticket! :title => 'Ticket #12', :description => 'Body'}
+      it { should be_an_instance_of ticket_class }
+    end
+    
+    context "when #save" do 
+      subject { project.ticket ticket_id }
+    end
+  end
+
   it "should be able to update and save a ticket" do
     pending
     @ticket = @project.ticket(1)
     @ticket.description = 'hello'
     @ticket.save.should == true
-  end
-
-  it "should be able to create a ticket" do 
-    pending
-    @ticket = @project.ticket!(:title => 'Ticket #12', :description => 'Body')
-    @ticket.should be_an_instance_of(@klass)
-    @ticket.project_id.should_not be_nil
   end
 
 end
