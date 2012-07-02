866aaf762fcee7e21d3a1a27bb6d724d80f712fc
Rafael George <george.rafael@gmail.com>
2012-07-02 20:12:32 +0000

    Initial refactor to comment specs * Adding more context and describe blocks to show intent on examples

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -6,63 +6,69 @@ describe TaskMapper::Provider::Redmine::Comment do
   let(:ticket_id)  { 1 }
   let(:comment_id) { 1 }
   let(:klass)      { TaskMapper::Provider::Redmine::Comment }
-  let(:taskmapper) { TaskMapper.new :redmine, :server => 'http://demo.redmine.org/', :token => 'abcdefghijk' }
-  let(:project)    { taskmapper.project(project_id) }
-
-  before(:all) do
-    headers = {'X-Redmine-API-Key' => 'abcdefghijk', 'Accept' => 'application/xml'}
-    headers_post_put = {'X-Redmine-API-Key' => 'abcdefghijk', 'Content-Type' => 'application/xml'}
-    ActiveResource::HttpMock.respond_to do |mock|
-      mock.get '/projects.xml', headers, fixture_for('projects'), 200
-      mock.get '/projects/1.xml', headers, fixture_for('projects/test-repo12'), 200
-      mock.get '/issues.xml?project_id=1', headers, fixture_for('issues'), 200
-      mock.get '/issues.xml', headers, fixture_for('issues'), 200
-      mock.get '/issues/1.xml?include=journals', headers, fixture_for('issues/1'), 200
-      mock.put '/issues/1.xml', headers_post_put, '', 200
-      mock.post '/issues.xml', headers_post_put, '', 200
-    end
-  end
+  let(:tm) { TaskMapper.new :redmine, :server => 'http://demo.redmine.org/', :token => 'abcdefghijk' }
+  let(:headers_auth) { {'X-Redmine-API-Key' => 'abcdefghijk' } }
+  let(:headers_get) { headers_auth.merge('Accept' => 'application/xml') }
+  let(:headers_post) { headers_auth.merge('Content-Type' => 'application/xml') }
 
   before(:each) do
     @ticket = project.ticket(ticket_id)
   end
 
+  describe "Retrieving comments" do 
+    before(:each) do 
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/projects/1.xml', headers_get, fixture_for('projects/test-repo12'), 200
+        mock.get '/issues/1.xml?include=journals', headers_get, fixture_for('issues/1'), 200
+      end
+    end
+    let(:project) { tm.project project_id }
+    let(:ticket) { project.ticket ticket_id }
+
+    context "when #comments" do 
+      subject { ticket.comments } 
+      it { should be_an_instance_of Array }
+    end
+  end
+
   it "should be able to load all comments" do
+    pending
     comments = @ticket.comments
     comments.size.should eq(2)
     comments.should be_an_instance_of(Array)
     comments.first.should be_an_instance_of(klass)
   end
-  
+
   it "should be able to load all comments based on 'id's" do
+    pending
     comments = @ticket.comments([comment_id])
     comments.should be_an_instance_of(Array)
     comments.first.should be_an_instance_of(klass)
     comments.first.id.should == comment_id
   end
-  
+
   it "should be able to load all comments based on attributes" do
+    pending
     comments = @ticket.comments(:id => comment_id)
     comments.should be_an_instance_of(Array)
     comments.first.should be_an_instance_of(klass)
   end
-  
-  it "should be able to load a comment based on id" do
+
+  it "shoultmd be able to load a comment based on id" do
+    pending
     comment = @ticket.comment(comment_id)
     comment.should be_an_instance_of(klass)
     comment.id.should == comment_id
   end
-  
+
   it "should be able to load a comment based on attributes" do
+    pending
     comment = @ticket.comment(:id => comment_id)
     comment.should be_an_instance_of(klass)
   end
-  
-  it "should return the class" do
-    @ticket.comment.should == klass
-  end
-  
+
   it "should be able to create a comment for a given task" do
+    pending
     comment = @ticket.comment!(:body => 'hello there boys and girls')
     comment.should be_an_instance_of(klass)
     comment.ticket_id.should_not be_nil
